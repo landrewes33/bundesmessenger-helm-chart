34804a89a3874160c6578bc302f56fc3792a72e5
Kai Alexander Hiller <kaialexander.hiller@bwi.de>
2025-04-10 08:44:49 +0000

    Regeneration of JSON Schema via CI job and Git pre-commit hook

--- scripts/gen-values-json.sh
+++ scripts/gen-values-json.sh
@@ -3,7 +3,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 # Generates the values.schema.json from values.schema.yaml
+# shellcheck disable=SC3040
+set -euo pipefail
 
+INFILE="${1:-values.schema.yaml}"
+[ "$INFILE" = "-" ] && INFILE=/dev/stdin
+
+# Transformations performed:
+# - Convert YAML to JSON
+# - Add auto-generation note
+# - Bundle external schemas
 yq --output-format=json \
     "{ \
         \"\$schema\": null, \
@@ -11,6 +20,6 @@ yq --output-format=json \
         \"\$comment\": \
             \"Do not modify by hand. This file is auto-generated from values.schema.yaml\" \
     } * ." \
-< values.schema.yaml \
+< "$INFILE" \
 | scripts/schema-tools.py bundle - \
 > values.schema.json
